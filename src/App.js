0311d5b054286311f28cd11f01aa1c73bbbcc627
Andre Dias <andreffdias@protonmail.com>
2021-02-25 16:06:13 +0000

    Chat: Inital Commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './styles/App.css';
 import { BrowserRouter as Router, Switch, Route/*, Link */} from 'react-router-dom';
 import Chess from './pages/Chess';
+import Chat from './pages/Chat';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Router>
         <Switch>
           <Route path='/chess' exact component={Chess}/>
+          <Route path='/chat' exact component={Chat}/>
         </Switch>
       </Router>
     </div>
