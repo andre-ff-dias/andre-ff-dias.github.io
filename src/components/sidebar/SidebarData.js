c8d637975905436eb455d8cf7973276729bb7c2e
Andre Dias <andreffdias@protonmail.com>
2021-02-28 14:06:05 +0000

    Sidebar: Menus Data

---
+++ src/components/sidebar/SidebarData.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as IoIcons from 'react-icons/io';
+import * as AiIcons from 'react-icons/ai';
+import * as RiIcons from 'react-icons/ri';
+
+export const SidebarData = [
+    {
+        title: 'Home',
+        path: '/',
+        icon: <AiIcons.AiFillHome />,
+    },
+    {
+        title: 'Portfolio',
+        path: '',
+        icon: <IoIcons.IoFolderOpenSharp />,
+        iconClosed: <RiIcons.RiArrowDownSFill />,
+        iconOpened: <RiIcons.RiArrowUpSFill />,
+        subNav: [
+            {
+                title: 'Chess',
+                path: '/chess',
+                icon: <FaIcons.FaChess />
+            },
+            {
+                title: 'Chat',
+                path: '/chat',
+                icon: <IoIcons.IoChatbubbles />
+            },
+            {
+                title: 'Todo',
+                path: '/todo',
+                icon: <RiIcons.RiTodoFill />
+            }
+        ]
+    }
+]
