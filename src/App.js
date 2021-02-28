9d6d73f913c6828f8c64b459e58683b8f1c86eca
Andre Dias <andreffdias@protonmail.com>
2021-02-28 14:47:24 +0000

    General: App Update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './styles/App.css';
-import { BrowserRouter as Router, Switch, Route/*, Link */} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Chess from './pages/Chess';
 import Chat from './pages/Chat';
 import TodoList from './pages/TodoList'
+import Sidebar from './components/sidebar/Sidebar';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Sidebar />
         <Switch>
           <Route path='/chess' exact component={Chess}/>
           <Route path='/chat' exact component={Chat}/>
