ab6bfe06b499c2be34414a4d331e60b244665c25
andre-ff-dias <andrefilipefd@hotmail.com>
2021-04-29 21:34:26 +0100

    Create FileData.js

---
+++ src/components/file/FileData.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { GoFileBinary } from "react-icons/go";
+
+export const FileData = [
+  {
+    name: "readme.md",
+    icon: <GoFileBinary />,
+    class: "file readme-exe",
+    file: "readMe",
+  },
+];
